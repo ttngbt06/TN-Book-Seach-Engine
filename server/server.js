1ae0c75d63224f57af89f2e5f9a0cc4c3aaf24d1
TOAN NGUYEN <ttngbt06@gmail.com>
2024-04-06 17:14:43 -0500

    Implement ApoloServer as middleware

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
 const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const startApolloServer = async () => {
+  await server.start();
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: authMiddleware,
+    })
+  );
+}
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
@@ -19,3 +35,6 @@ app.use(routes);
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+const PORT = process.env.PORT || 3001;
